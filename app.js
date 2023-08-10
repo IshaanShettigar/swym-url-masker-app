24058c8176a5f1be419a2e4056e23e463e583002
Ishaan Shettigar <ishaanshettigar@gmail.com>
2023-08-10 15:33:35 +0530

    second commit

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const express = require("express")
 const Main = require('./models/mainSchema')
 const connectDB = require("./db/connect");
 const app = express()
-
+const cors = require('cors');
+const mongoose = require('mongoose')
+app.use(cors());
 app.use(express.json())
+const crypto = require('crypto')
+
+
+app.use(express.static('./frontend'));
 
 
 app.post('/sendURLs', async (req, res) => {
@@ -13,18 +19,18 @@ app.post('/sendURLs', async (req, res) => {
     let newURLs = {
     }
     for (let i = 0; i < urls.length; i++) {
-        newURLs[urls[i]] = `ishaan@swym${i}`
+        newURLs[urls[i]] = `slug${i}`
         try {
-            await Main.create({ original: urls[i], new_url: `ishaan@swym${i}`, clicks: 0 })
+            await Main.create({ original: urls[i], new_url: newURLs[urls[i]], clicks: 0 })
         }
         catch (error) {
-            if (error instanceof mongoose.Error.ValidationError) {
-                // Handle validation errors
-                return res.status(400).json({ error: 'Validation error', details: error.errors });
-            } else if (error instanceof mongoose.Error.MongoServerError) {
-                // Handle MongoServerError
-                return res.status(500).json({ error: 'MongoDB server error', details: error.message });
-            }
+            // if (error instanceof mongoose.Error.ValidationError) {
+            //     // Handle validation errors
+            //     return res.status(400).json({ error: 'Validation error', details: error.errors });
+            // } else if (error instanceof mongoose.Error.MongoServerError) {
+            //     // Handle MongoServerError
+            //     return res.status(500).json({ error: 'MongoDB server error', details: error.message });
+            // }
             console.log(error);
             return res.status(500).json({ error: 'Internal server error' });
         }
@@ -33,20 +39,50 @@ app.post('/sendURLs', async (req, res) => {
     res.status(200).json(newURLs)
 })
 
+app.get('/urls', async (req, res) => {
+    try {
+        console.log("------------FETCHING-------------");
+        let data = await Main.find({})
+        const filteredData = data.map(item => ({
+            original: item.original,
+            new_url: item.new_url,
+            clicks: item.clicks
+        }));
+        console.log(filteredData);
+        res.status(200).json({ "data": filteredData })
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
+
 app.get('/:slug', async (req, res) => {
+    console.log("------REDIRECTING-------------");
     let slug = req.params.slug;
     // res.status(200).json({ newUrl: "test" })
     // check if slug in database else return error
+    console.log("SLUG", slug);
     try {
         let original_url = await Main.findOne({ new_url: slug })
-        console.log(original_url);
-        res.redirect(original_url.original)
+        console.log(original_url.clicks);
+        await Main.updateOne({ new_url: slug, clicks: original_url.clicks + 1 })
+        // console.log(new URL(original_url));
+        res.redirect(`https://${original_url.original}`)
+
+        // res.redirect(new URL(original_url))
     }
     catch {
         console.log("Error retreving url")
         res.status(404).send("URL NOT IN DATABASE")
     }
 })
+
+
+
+// app.get('/', (req, res) => {
+//     console.log(crypto.randomUUID());
+// })
+
 
 
 
---
+++ frontend/script.js
@@ -0,0 +1,125 @@
+const fileUploadBtn = document.getElementById('upload-csv-btn')
+const fileUpload = document.getElementById('csv-file')
+
+
+/**
+ * Need to strip the data as well. Take care of valid and invalid urls via a regex. If there were some invalid urls then tell the
+ * user which one was invalid. 
+ */
+function readCSV() {
+    let files = fileUpload.files
+    if (files.length == 0) {
+        alert("No file uploaded")
+        return
+    }
+
+    var file = files[0];
+    // FileReader Object
+    var reader = new FileReader();
+    // Read file as string 
+    reader.readAsText(file);
+
+    reader.onload = async function (event) {
+
+        var csvdata = event.target.result;
+        // Split by line break to gets rows Array
+
+        var rowData = csvdata.split('\n');
+        var newRowData = []
+        for (let i = 0; i < rowData.length; i++) {
+            rowData[i] = rowData[i].trim()
+            if (rowData[i] != "") {
+                newRowData.push(rowData[i])
+            }
+        }
+        // console.log(rowData);
+        let body = {
+            "urls": newRowData
+        }
+        console.log(rowData);
+        try {
+            const response = await fetch("http://localhost:5000/sendURLs",
+                {
+                    method: "POST",
+                    body: JSON.stringify(body),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+            if (!response.ok) {
+                const errorResponse = await response.json(); // Parse the error response body
+                console.log("Server returned an error:", errorResponse);
+                // Display the error message in an alert box
+                alert("An error occurred: " + errorResponse.message);
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+    }
+}
+
+fileUploadBtn.addEventListener('click', readCSV)
+
+
+/* To fetch the data */
+
+
+function populateTable(data) {
+    const tableBody = document.querySelector("#dashboard-table tbody");
+
+    // Clear existing rows
+    tableBody.innerHTML = "";
+
+    data.forEach(item => {
+        const row = tableBody.insertRow();
+        const originalCell = row.insertCell(0);
+        const newUrlCell = row.insertCell(1);
+        const clicksCell = row.insertCell(2);
+
+        // Create <a> tags for original and masked URLs        
+        const originalLink = document.createElement("a");
+        originalLink.href = item.original;
+        originalLink.target = "_blank"; // Open in new tab
+        originalLink.textContent = item.original;
+
+        const newUrlLink = document.createElement("a");
+        newUrlLink.href = item.new_url;
+        newUrlLink.target = "_blank"; // Open in new tab
+        newUrlLink.textContent = `${item.new_url}`;
+
+
+        originalCell.appendChild(originalLink);
+        newUrlCell.appendChild(newUrlLink);
+
+        clicksCell.textContent = item.clicks;
+        console.log(item.new_url, item.original);
+    });
+}
+
+
+
+const fetchBtn = document.getElementById('fetch-data')
+
+fetchBtn.addEventListener("click", async () => {
+    try {
+        const response = await fetch("http://localhost:5000/urls", {
+            method: "GET",
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            populateTable(data.data);
+        } else {
+            console.error("Error fetching data");
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
+
+/* What about the single text box to check just one url */
